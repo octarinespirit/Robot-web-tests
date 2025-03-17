d7f51b991d3a10ef5d184d282b2df2ffb2960fbb
Joonas Symbio <joonas.malila@symbio.com>
2025-03-17 13:56:33 +0200

    Adding first DNS tests, record lookup and reverse

---
+++ keywords/dnskeywords.py
@@ -0,0 +1,27 @@
+"""Module to check DNS records"""
+import dns.resolver
+import dns.reversename
+from robot.api.deco import keyword
+
+@keyword
+def get_dns_record(domain, record):
+    """Fetches the DNS record for a given domain and record type from Google's nameserver"""
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ["8.8.8.8"]
+    try:
+        response = resolver.resolve(domain, record)
+        return [rdata.to_text() for rdata in response]
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.exception.Timeout) as e:
+        return "Something went wrong, could not get the record. Error: ", str(e)
+
+@keyword
+def check_reverse_name(ip):
+    """Check's the given ip address' reverse name, the domain name"""
+    for address in ip:
+        try:
+            rev_name = dns.reversename.from_address(address)
+            print(rev_name)
+            return rev_name
+        except:
+            print("Something went wrong")
+            return "Error"
--- tests/Connectivity/DNS/dns.robot
+++ tests/Connectivity/DNS/dns.robot
@@ -1,4 +1,27 @@
 *** Settings ***
+Library    Dialogs
+Library    SeleniumLibrary
+Library    ../../../keywords/dnskeywords.py
+Resource    ../../../keywords/keywords.resource
 
-# TO DO: use needed libraries
-# Create dns service tests to check dns records
+
+*** Test Cases ***
+Check DNS records
+    [Documentation]    Check the given DNS record of the given domain
+
+    ${URL}     Get Value From User   Enter the website URL:
+    ${DATA}    Get Value From User    Enter the record to be fetched:
+    ${RECORD}    Get Dns Record    ${URL}    ${DATA}
+    Log    ${RECORD}
+
+Check The Reverse Name Of IP Address
+    [Documentation]    Checks the reverse name of an ip address, the domain name.
+
+    ${URL}     Get Value From User   Enter the website URL:
+    ${RECORD}    Get Dns Record    ${URL}    A
+    FOR    ${IP}    IN    ${RECORD}
+        Log    ${IP}
+    END
+    ${NAME}    Check Reverse Name    ${IP}
+    Log    ${NAME}
+    Should Be Equal As Strings    ${URL}    ${NAME}
